a33f1284ffffece4258b37c67898d6fffc4d68a7
SNEHAL SAWANT <snellbhor@gmail.com>
2026-06-05 11:14:15 +0530

    Add files via upload

---
+++ C-Programming/Practice/program70.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+   int iCnt = 0;
+
+   for(iCnt = 1; iCnt <=5; iCnt++)
+   {
+      printf("Sarvadnya\n");
+   }
+   
+   return 0;
+}
---
+++ C-Programming/Practice/program72.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+   int iCnt = 0;
+
+   iCnt = 1;
+
+   do
+   {
+      printf("Sarvadnya\n");
+      iCnt++;
+      
+   }while(iCnt <= 5);
+
+   return 0;
+}
