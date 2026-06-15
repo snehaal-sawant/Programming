8376a02c3de5d78d78d17cd2b6017a5f90fa97ac
SNEHAL SAWANT <snellbhor@gmail.com>
2026-06-15 22:10:43 +0530

    Add files via upload

---
+++ Java-Programming/Practice/program145.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class program145
+{
+   public static void Update(int Arr[])
+   {
+      int iCnt = 0;
+
+      for(iCnt = 0; iCnt < Arr.length; iCnt++)
+      {
+         Arr[iCnt]++;
+      }
+   }
+   public static void main(String A[]) 
+   {
+      Scanner sobj = new Scanner(System.in);
+      int iCnt = 0;
+
+      System.out.println("Enter Number of Elements : ");
+      int iLength = sobj.nextInt();
+
+      int Brr[] = new int[iLength];
+
+      System.out.println("Enter the Elements : ");
+
+      for(iCnt = 0; iCnt < Brr.length; iCnt++)
+      {
+         Brr[iCnt] = sobj.nextInt();
+      }
+      
+      System.out.println("Array elements before function call :");
+      for(iCnt = 0; iCnt < Brr.length; iCnt++)
+      {
+         System.out.println(Brr[iCnt]);
+      }
+
+      Update(Brr);
+
+      System.out.println("Array elements after function call :");
+      for(iCnt = 0; iCnt < Brr.length; iCnt++)
+      {
+         System.out.println(Brr[iCnt]);
+      }
+      
+      Brr = null;
+      System.gc();
+      
+      sobj.close();
+   }
+}
---
+++ Java-Programming/Practice/program146.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class ArrayX 
+{
+   public static void Update(int Arr[])
+   {
+      int iCnt = 0;
+      for(iCnt = 0; iCnt < Arr.length; iCnt++)
+      {
+         Arr[iCnt]++;
+      }
+   }
+}
+
+public class program146
+{
+   public static void main(String A[]) 
+   {
+      Scanner sobj = new Scanner(System.in);
+      int iCnt = 0;
+
+      System.out.println("Enter Number of Elements : ");
+      int iLength = sobj.nextInt();
+
+      int Brr[] = new int[iLength];
+
+      System.out.println("Enter the Elements : ");
+
+      for(iCnt = 0; iCnt < Brr.length; iCnt++)
+      {
+         Brr[iCnt] = sobj.nextInt();
+      }
+      
+      System.out.println("Array elements before function call :");
+      for(iCnt = 0; iCnt < Brr.length; iCnt++)
+      {
+         System.out.println(Brr[iCnt]);
+      }
+
+      ArrayX.Update(Brr);
+
+      System.out.println("Array elements after function call :");
+      for(iCnt = 0; iCnt < Brr.length; iCnt++)
+      {
+         System.out.println(Brr[iCnt]);
+      }
+      
+      Brr = null;
+      System.gc();
+      
+      sobj.close();
+   }
+}
