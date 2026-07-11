d984a9fd295c09b98e82b9f9265ef156b833b5ba
SNEHAL SAWANT <snellbhor@gmail.com>
2026-07-11 20:12:42 +0530

    Add files via upload

---
+++ C++-Programming/Practice/program412.cpp
@@ -0,0 +1,296 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+struct node
+{
+   int data;
+   struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+
+class SinglyLL          //Singly Linear Linklist 
+{
+   private:
+      PNODE first;
+      int iCount; 
+
+   public:
+      SinglyLL();
+      void Display();
+      int Count();
+      void InsertFirst(int iNo);
+      void InsertLast(int iNo);
+      void InsertAtPos(int iNo, int iPos);
+      void DeleteFirst();
+      void DeleteLast();
+      void DeleteAtPos(int iPos);
+};
+
+SinglyLL :: SinglyLL()
+{
+   this->first = NULL;
+   this->iCount = 0;
+}
+
+void SinglyLL :: Display()
+{
+   PNODE temp = NULL;
+   temp = this->first;
+
+   while(temp != NULL)
+   {
+      cout << "| "<<temp->data<<" | -> ";
+      temp = temp->next;
+   }
+
+   cout<<"NULL"<<endl;
+}
+
+int SinglyLL :: Count()
+{
+   return this->iCount;
+}
+
+void SinglyLL :: InsertFirst(int iNo)
+{
+   PNODE newn = NULL;
+
+   newn = new NODE;        //Memory Allocated
+   newn->data = iNo;
+   newn->next = NULL;
+
+   if(this->first == NULL)
+   {
+      this->first = newn;
+   }
+   else
+   {
+      newn->next = this->first;
+      this->first=newn;
+   }
+
+   this->iCount++;            //IMp - Increments counter by 1 whenever new node is inserted
+}
+
+void SinglyLL :: InsertLast(int iNo)
+{
+   PNODE newn = NULL;
+   PNODE temp = NULL;
+
+   newn = new NODE;        //Memory Allocated
+   
+   newn->data = iNo;
+   newn->next = NULL;
+
+   if(this->first == NULL)
+   {
+      this->first = newn;
+   }
+   else
+   {
+      temp = this->first;
+
+      while(temp->next != NULL)
+      {
+         temp = temp->next;
+      }
+
+      temp->next = newn;
+   }
+   
+   this->iCount++;
+}
+
+void SinglyLL :: InsertAtPos(int iNo, int iPos)
+{
+   int i = 0;
+
+   PNODE temp = NULL;
+   PNODE newn = NULL;
+
+   if((iPos < 1) || (iPos > this->iCount+1))
+   {
+      cout << "Invalid Position"<<endl;
+      return;
+   }
+
+   if(iPos == 1)
+   {
+      this->InsertFirst(iNo);       // Calling our own function from the same class
+   }
+   else if(iPos == iCount+1)
+   {
+      this->InsertLast(iNo);
+   }
+   else
+   {
+      newn = new NODE;
+
+      newn->data = iNo;
+      newn->next = NULL;
+
+      temp = this->first;
+
+      for(i = 0; i < iPos-1; i++)
+      {
+         temp = temp->next;
+      }
+
+      newn->next = temp->next;
+      temp->next = newn;
+
+      this->iCount++;
+   }
+}
+
+void SinglyLL :: DeleteFirst()
+{
+   PNODE temp = NULL;
+
+   if(this->first == NULL)
+   {
+      return;
+   }
+   else if(this->first->next == NULL)
+   {
+      delete this->first;
+   }
+   else
+   {
+      temp = this->first;
+      this->first = this->first->next;
+      delete temp;
+   }
+   iCount--;
+}
+
+void SinglyLL :: DeleteLast()
+{
+   PNODE temp = NULL;
+
+   if(this->first == NULL)
+   {
+      return;
+   }
+   else if(this->first->next == NULL)
+   {
+      delete this->first;
+   }
+   else
+   {
+      temp = this->first;
+
+      while(temp->next->next != NULL)
+      {
+         temp = temp->next;
+      }
+
+      delete temp->next;
+      temp->next = NULL;
+   }
+   iCount--;
+}
+
+void SinglyLL :: DeleteAtPos(int iPos)
+{
+   int i = 0;
+
+   PNODE temp = NULL;
+   PNODE target = NULL;
+
+   if((iPos < 1) || (iPos > this->iCount))
+   {
+      cout << "Invalid Position"<<endl;
+      return;
+   }
+
+   if(iPos == 1)
+   {
+      this->DeleteFirst();       // Calling our own function from the same class
+   }
+   else if(iPos == iCount)
+   {
+      this->DeleteLast();
+   }
+   else
+   {
+      temp = this->first;
+      
+      for(i = 0; i < iPos-1; i++)
+      {
+         temp = temp->next;
+      }
+      
+      target = temp->next;
+
+      temp->next = target->next;          //temp->next = temp->next->next;
+
+      delete target;
+
+      this->iCount--;
+   }
+}
+
+int main()
+{
+   int iRet = 0;
+
+   SinglyLL sobj;
+
+   sobj.InsertFirst(51);
+   sobj.InsertFirst(21);
+   sobj.InsertFirst(11);
+
+   sobj.Display();
+   
+   iRet = sobj.Count();
+
+   cout<<"Number of elements are : "<< iRet << endl;
+
+   sobj.InsertLast(101);
+   sobj.InsertLast(111);
+   sobj.InsertLast(121);
+
+   sobj.Display();
+   
+   iRet = sobj.Count();
+
+   cout<<"Number of elements are : "<< iRet << endl;
+
+   sobj.DeleteFirst();
+
+   sobj.Display();
+   
+   iRet = sobj.Count();
+
+   cout<<"Number of elements are : "<< iRet << endl;
+
+   sobj.DeleteLast();
+
+   sobj.Display();
+   
+   iRet = sobj.Count();
+
+   cout<<"Number of elements are : "<< iRet << endl;
+
+   sobj.InsertAtPos(105, 4);
+
+   sobj.Display();
+   
+   iRet = sobj.Count();
+
+   cout<<"Number of elements are : "<< iRet << endl;
+
+   sobj.DeleteAtPos(4);
+
+   sobj.Display();
+   
+   iRet = sobj.Count();
+
+   cout<<"Number of elements are : "<< iRet << endl;
+
+   return 0;
+}
