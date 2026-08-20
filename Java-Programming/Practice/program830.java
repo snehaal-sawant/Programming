bb08056d324cd5e15081d5141f8fc7023d9830d4
unknown <snellbhor@gmail.com>
2026-08-20 18:05:03 +0530

    Add program830.java

---
+++ Java-Programming/Practice/program830.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class program830
+{
+   public static void main(String[] args)
+   {
+      ArrayList <Integer>aobj = new ArrayList<Integer>(); 
+      
+      aobj.add(11);
+      aobj.add(51);
+      aobj.add(21);
+      aobj.add(101);
+      aobj.add(51);
+
+      System.out.println(aobj); //gives value at index 0
+   }
+}
